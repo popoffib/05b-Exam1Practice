ae45aa92a3689101a5d194a8780c62e9f36e2d4f
popoffib <popoffib@rose-hulman.edu>
2019-02-04 15:35:23 -0500

    added tests

--- src/problem1.py
+++ src/problem1.py
@@ -199,10 +199,11 @@ def problem1a(m, n):
 
     return total
 
+
 def run_test_problem1b():
     """ Tests the   problem1b   function. """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement this TEST function.
+    # DONE: 5. Implement this TEST function.
     #   It TESTS the  problem1b  function defined below.
     #   Include at least **   4   ** tests.  Use the usual form:
     #
@@ -220,6 +221,30 @@ def run_test_problem1b():
     print('Testing the   problem1b   function:')
     print('--------------------------------------------------')
 
+    # Test 1:
+    expected = 5
+    answer = problem1b(3, 5)
+    print('Test 1 expected:', expected)
+    print('       actual:', answer)
+
+    # Test 2:
+    expected = 1
+    answer = problem1b(2, 1)
+    print('Test 2 expected:', expected)
+    print('       actual:', answer)
+
+    # Test 3:
+    expected = 44
+    answer = problem1b(5, 40)
+    print('Test 3 expected:', expected)
+    print('       actual:', answer)
+
+    # Test 4:
+    expected = 7
+    answer = problem1b(5, 5)
+    print('Test 4 expected:', expected)
+    print('       actual:', answer)
+    
 
 def problem1b(m, f):
     """
